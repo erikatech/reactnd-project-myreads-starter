8df42ea9a0cc62efdbf202853904fa69de3b881d
Erika Silva <erika.silva@itriad.org.br>
2017-09-30 08:44:51 -0400

    refactor: change the way the books list is updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookShelf from "./BookShelf";
+import BookSearch from "./BookSearch";
 
-class BooksApp extends React.Component {
-	state = {
-		books: []
-	};
+class BooksApp extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			books: []
+		}
+	}
 
 	componentDidMount() {
 		BooksAPI.getAll().then((books) => {
@@ -14,15 +20,24 @@ class BooksApp extends React.Component {
 		})
 	}
 
-	moveBook = (e, book) => {
-		console.log(e);
-		console.log(book);
-
-	}
+	moveBook = (shelf, book) => {
+		if (shelf !== book.shelf) {
+			BooksAPI.update(book, shelf).then(() => {
+				this.setState({books:
+				  this.state.books
+				    .map(_ => {
+				    	if(_.id === book.id){
+				    		_.shelf = shelf;
+					    }
+					    return _;
+				    })});
+			});
+		}
+	};
 
 	render() {
-		const { books } = this.state;
-		const sections = [
+		const {books} = this.state;
+		const shelves = [
 			{title: 'Currently Reading', shelf: "currentlyReading", books},
 			{title: 'Want to Read', shelf: 'wantToRead', books},
 			{title: 'Read', shelf: 'read', books}
@@ -30,21 +45,34 @@ class BooksApp extends React.Component {
 
 		return (
 		  <div className="app">
-			  <div className="list-books">
-				  <div className="list-books-title">
-					  <h1>MyReads</h1>
-				  </div>
-				  {sections.map(_ => (
-					<BookShelf
-					  key={_.title}
-					  shelf={_.shelf}
-					  title={_.title}
-					  books={_.books}/>
-				  ))}
-			  </div>
-			  <div className="open-search">
-				  <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-			  </div>
+			  <Route exact path="/" render={() => (
+				<div className="list-books">
+					<div className="list-books-title">
+						<h1>MyReads</h1>
+					</div>
+					{shelves.map(_ => (
+					  <BookShelf
+						key={_.title}
+						shelf={_.shelf}
+						title={_.title}
+						books={_.books}
+						moveBook={this.moveBook}
+					  />
+					))}
+					<div className="open-search">
+						<Link
+						  to="/search"
+						  className="add-contact">
+							Add a book
+						</Link>
+					</div>
+				</div>
+			  )}/>
+
+
+			  <Route exact path="/search" render={() => (
+				<BookSearch />
+			  )}/>
 		  </div>
 		)
 	}
