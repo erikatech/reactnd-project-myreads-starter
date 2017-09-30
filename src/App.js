ab7674934e671c392f1e5086ba84468c5c34235f
Erika Silva <erika.silva@itriad.org.br>
2017-09-30 11:46:27 -0400

    refactor: improve the way the book gets updated after the update process

--- src/App.js
+++ src/App.js
@@ -2,49 +2,72 @@ import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import * as BooksAPI from './BooksAPI'
-import './App.css'
+import './App.css';
 import BookShelf from "./BookShelf";
 import BookSearch from "./BookSearch";
 
+/**
+ * Represents the main page with all the shelves
+ */
 class BooksApp extends Component {
+	// initializes the books
 	state = {
 		books: []
 	};
 
 	componentDidMount() {
+		// send a request to BooksAPI the get my current books
 		BooksAPI.getAll().then((books) => {
+			// updates the current state
 			this.setState({books});
 		})
 	}
 
-	moveBook = (shelf, book) => {
-		if (shelf !== book.shelf) {
+	/**
+	 * Moves a book to a specific shelf
+	 * @param book
+	 * @param shelf
+	 */
+	moveBook = (book, shelf) => {
+		// if the user selects the current book shelf, it doesn't need to perform the update
+		if (book.shelf !== shelf) {
 			BooksAPI.update(book, shelf).then(() => {
 				this.arrangeBooks(book, shelf);
 			});
 		}
 	};
 
+	/**
+	 * Organize a book according to its shelf after the updating process
+	 * @param book
+	 * @param shelf
+	 */
 	arrangeBooks = (book, shelf) => {
+		// checking if the book exists in the current state
 		const index = this.state.books.findIndex(currentBook => currentBook.id === book.id);
 		let books = this.state.books;
-		if (index > 0) {
-			books = this.state.books
-			  .map(_ => {
-				  if (_.id === book.id) {
-					  _.shelf = shelf;
-				  }
-				  return _;
-			  });
-		} else {
+
+		if (index > 0) { // if the book exists
+			// maps through the current state books to modify its shelf according to user
+			// modification (passed as parameter)
+			books.filter(_ => _.id === book.id)[0].shelf = shelf;
+
+		} else { // if it doesn't exists yet
+			// sets the book shelf
 			book.shelf = shelf;
+
+			// add to current books
 			books.push(book);
 		}
+
+		// finally update the state
 		this.setState({books});
 	};
 
 	render() {
+		// creates a constant that represents the books of current state
 		const {books} = this.state;
+		// creates the three shelves
 		const shelves = [
 			{title: 'Currently Reading', shelf: "currentlyReading", books},
 			{title: 'Want to Read', shelf: 'wantToRead', books},
@@ -69,8 +92,7 @@ class BooksApp extends Component {
 					))}
 					<div className="open-search">
 						<Link
-						  to="/search"
-						  className="add-contact">
+						  to="/search">
 							Add a book
 						</Link>
 					</div>
