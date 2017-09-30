ef24de4b679cf75c84f2fd8209319209fb97be76
Erika Silva <erika.silva@itriad.org.br>
2017-09-30 11:46:45 -0400

    refactor: add comments

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -3,27 +3,47 @@ import BookShelf from './BookShelf';
 import * as BooksAPI from './BooksAPI';
 import SearchBar from './SearchBar';
 
+/**
+ * Component that represents the search page
+ * It receive as props the currentBooks that are defined in App.js
+ *  so we can know which searched books are already added to some shelf
+ * It also receive as props the moveBook method, which will be passed to BookShelf component
+ */
 class BookSearch extends Component {
 
+	// initializes the book list
 	state = {
 		books: []
 	};
 
+	/**
+	 * Search a book according to the user input
+	 * @param term the user input value
+	 */
 	bookSearch = (term) => {
 		BooksAPI.search(term, 20).then((result) => {
+			// if the result return error, it means it didn't find any book
 			if(result.error){
+				// so the book state will be set to an empty array
 				this.setState({books: []});
 			} else {
+				// we need to check if some book inside result
+				// is already added to a shelf
 				this.props.currentBooks.forEach(currentBook => {
 					result
 					  .filter(book => book.id === currentBook.id)
 					  .map(_ => _.shelf = currentBook.shelf);
 				});
+
+				// updates the current state
 				this.setState({books: result});
 			}
 		})
-
 	};
+
+
+
+
 
 	render() {
 		return (
